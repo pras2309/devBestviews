764c45919a4794da28a4c1bc4e1766bdafbe17a1
Prashant Agarwal <prashant.india@gmail.com>
2019-06-04 14:27:48 +0530

    LiveZila chatbot adding;

---
+++ livezilla/_config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+/*********************************************************************************
+* LiveZilla config.php
+*
+* Copyright 2016 LiveZilla GmbH
+* All rights reserved.
+* LiveZilla is a registered trademark.
+*
+********************************************************************************/
+
+$_CONFIG['gl_pr_cr'] = '1559637536';
+$_CONFIG['gl_lzid'] = '3efaad48ce';
+$_CONFIG['b64'] = false;
+
+$_CONFIG[0]['gl_db_host'] = 'localhost';
+$_CONFIG[0]['gl_db_user'] = 'localuser';
+$_CONFIG[0]['gl_db_ext'] = 'mysqli';
+$_CONFIG[0]['gl_db_eng'] = 'InnoDB';
+$_CONFIG[0]['gl_db_pass'] = 'R@5gull@';
+$_CONFIG[0]['gl_db_name'] = 'wp_bestviews';
+$_CONFIG[0]['gl_db_prefix'] = 'lz_';
+
+?>
